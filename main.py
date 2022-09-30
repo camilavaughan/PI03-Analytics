0ea91502283fd1173aeed4db7ab259a73f53514d
Camila Vaughan <camiivaughan@gmail.com>
2022-09-30 11:43:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ def get_market_data(coin):
     df["date"] = pd.to_datetime(df["startTime"]).dt.date
     df = df.drop(columns=["startTime", "time"])
     return df
+
+#Configuramos la página
+st.set_page_config(page_title= "camilavaughan", layout= "wide")
+st.markdown(''' # **FTX Price App**
+A simple cryptocurrency price app pulling price data from FTX API.
+''')
+
+coins = ["BTC", "ETH", "USDT", "BNB", "XRP", "SOL", "DOGE", "DOT", "DAI", "SHIB"] 
+coin = st.sidebar.selectbox("Choose a coin: ", coins)
+
+url = f"{api_url}/markets/{coin}/USD"
+request = requests.get(url).json()
+data = pd.Series(request["result"])
+
+col1, col2, col3, col4 = st.columns(4)
+price = data["price"]
+col1.metric("Price", data["price"])
+col2.metric("Low", data["priceLow24h"])
+col3.metric("High", data["priceHigh24h"])
+col4.metric("Volume", int(data["volumeUsd24h"]))
